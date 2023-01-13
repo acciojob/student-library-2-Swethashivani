dbb1490c45b510d70f4df0556ea78965f0c7a2bd
Swetha Shivani <cnk.swetha@gmail.com>
2023-01-13 20:24:38 +0530

    added controller layer

--- src/main/java/com/driver/controller/AuthorController.java
+++ src/main/java/com/driver/controller/AuthorController.java
@@ -5,7 +5,19 @@ package com.driver.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/author")
 public class AuthorController {
 
     //Write createAuthor API with required annotations
+
+    @Autowired
+    AuthorService authorService;
+
+    @PostMapping("/")
+    public ResponseEntity createAuthor(@RequestBody Author author){
+
+        authorService.create(author);
+        return new ResponseEntity<>("the author is successfully added to the system", HttpStatus.CREATED);
+    }
+
 }
