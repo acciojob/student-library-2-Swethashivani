296e70d6c897d626c4add905f652c5acdaf80868
Swetha Shivani <cnk.swetha@gmail.com>
2023-01-13 21:08:36 +0530

    fixed minor changes

--- src/main/java/com/driver/models/Book.java
+++ src/main/java/com/driver/models/Book.java
@@ -2,17 +2,11 @@ package com.driver.models;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
 
-
 @Entity
-
-
 public class Book {
 
     @Id
@@ -26,7 +20,7 @@ public class Book {
 
     @ManyToOne
     @JoinColumn
-    @JsonIgnoreProperties("booksWritten")
+    @JsonIgnoreProperties("books")
     private Author author;
 
     @ManyToOne
@@ -38,18 +32,26 @@ public class Book {
     @Column(columnDefinition = "TINYINT(1)")
     private boolean available;
 
-    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnoreProperties("book")
     private List<Transaction> transactions;
 
     public Book() {
     }
 
-    public Book(String name, Genre genre, Author author, Card card) {
+    public Book(String name, Genre genre, Author author) {
         this.name = name;
         this.genre = genre;
         this.author = author;
-        this.card = card;
+        this.available = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -68,12 +70,12 @@ public class Book {
         this.genre = genre;
     }
 
-    public Author getAuthor() {
-        return author;
+    public boolean isAvailable() {
+        return available;
     }
 
-    public void setAuthor(Author author) {
-        this.author = author;
+    public void setAvailable(boolean available) {
+        this.available = available;
     }
 
     public Card getCard() {
@@ -83,5 +85,21 @@ public class Book {
     public void setCard(Card card) {
         this.card = card;
     }
-}
 
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+}
